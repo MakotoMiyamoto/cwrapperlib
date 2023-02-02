22f506f2e696b5e993d13af2387aaa289ae10ac9
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2023-02-02 04:39:53 -0600

    Write result handler, testing

--- main.c
+++ main.c
@@ -4,20 +4,15 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifdef _WIN32
-    #define __scanf_indpt(...) scanf_s(__VA_ARGS__)
-    #define __fscanf_indpt(...) fscanf_s(__VA_ARGS__)
-    #define __sscanf_indpt(...) sscanf_s(__VA_ARGS__)
-#else
-    #define __scanf_indpt(...) scanf(__VA_ARGS__)
-    #define __fscanf_indpt(...) fscanf(__VA_ARGS__)
-    #define __sscanf_indpt(...) sscanf_s(__VA_ARGS__)
+    #define scanf(...) scanf_s(__VA_ARGS__)
+    #define fscanf(...) fscanf_s(__VA_ARGS__)
+    #define sscanf(...) sscanf_s(__VA_ARGS__)
+    #define strcpy(...) strcpy_s(__VA_ARGS__)
+    #define strncpy(...) strncpy_s(__VA_ARGS__)
 #endif
-
-#define scanf(...) __scanf_indpt(__VA_ARGS__)
-#define fscanf(...) __fscanf_indpt(__VA_ARGS__)
-#define sscanf(...) __sscanf_indpt(__VA_ARGS__)
 
 #define nameof(FUNC) #FUNC
 
@@ -42,7 +37,7 @@ void _raise(const char *filename, const char *function, int line, const char *re
 
 int _read_int(FILE *p, int _line, const char *_filename) {
     int d;
-    int res = __fscanf_indpt(p, "%d", &d);
+    int res = fscanf(p, "%d", &d);
     if (res != __READ_INT_READLEN) {
         _raise(_filename, "read_int(FILE *)", _line, "scanf_s read failure.");
     }
@@ -51,7 +46,7 @@ int _read_int(FILE *p, int _line, const char *_filename) {
 
 double _read_double(FILE *p, int _line, const char *_filename) {
     double lf;
-    int res = __fscanf_indpt(p, "%lf", &lf);
+    int res = fscanf(p, "%lf", &lf);
     if (res != __READ_DOUBLE_READLEN) {
         _raise(_filename, "read_double(FILE *)", _line, "fscanf read failure.");
     }
@@ -60,7 +55,7 @@ double _read_double(FILE *p, int _line, const char *_filename) {
 
 char _read_char(FILE *p, int _line, const char *_filename) {
     char c;
-    int res = __fscanf_indpt(p, "%c", &c);
+    int res = fscanf(p, "%c", &c);
     if (res != __READ_CHAR_READLEN) {
         _raise(_filename, "read_char(FILE *)", _line, "fscanf read failure.");
     }
@@ -110,65 +105,60 @@ char _read_char(FILE *p, int _line, const char *_filename) {
 /// Note: This is not to be confused with a byte.
 #define read_char() read_char_f(stdin)
 
-// #define unnamed(type, err) \
-// enum type##_try { \
-//     Ok(type), \
-//     Err(err) \
-// };
+#define Generic__ResultUnion(TYPE)  \
+typedef union ResultUnion__##TYPE { \
+    TYPE ok;                        \
+    char err[256];                  \
+} ResultUnion__##TYPE;
 
-// unnamed(int, const char *)
+#define ResultUnion(TYPE) ResultUnion__##TYPE
 
-union ResultU {
-    int ok;
-    char err[256];
-};
+#define Generic__Result(TYPE)   \
+Generic__ResultUnion(TYPE)      \
+                                \
+typedef struct Result_##TYPE {  \
+    bool _err;                  \
+    ResultUnion(TYPE) _data;    \
+} Result_##TYPE;                \
+                                \
+Result(TYPE) Result_##TYPE##__Ok(TYPE t) { \
+    Result(TYPE) res = { ._err = false, ._data.ok = t }; \
+    return res; \
+} \
+Result(TYPE) Result_##TYPE##__Err(const char *err_msg) { \
+    Result(TYPE) res = { ._err = true }; \
+    strcpy(res._data.err, sizeof res._data.err, err_msg); \
+    return res; \
+} \
+TYPE Result_##TYPE##_unwrap(Result(TYPE) *res, const char *filename, int line) { \
+    if (res->_err) { _raise(filename, "unwrap(?)", line, "Result not Ok."); } \
+    return res->_data.ok; \
+} \
 
-bool is_err(union ResultU *res) {
-    return res->err[255] == '\1';
-}
+#define Result(TYPE) Result_##TYPE
 
-typedef struct _Result {
-    bool err_flag;
-    union ResultU __res;
-} Result;
+#define Ok(TYPE, t) Result_##TYPE##__Ok(t)
+#define Err(TYPE, msg) Result_##TYPE##__Err(msg)
 
-int Ok(Result *res) {
-    if (res->err_flag == 1) {
-        _raise(__FILE__, "none()", __LINE__, "Result not Ok.");
+#define unwrap(TYPE, p_res) Result_##TYPE##_unwrap(p_res, __FILE__, __LINE__)
+#define is_err(res) res._err == true
+
+Generic__Result(int)
+
+Result(int) read_int_range(int lower, int upper) {
+    int t;
+    scanf("%d", &t);
+    if (t < lower || t > upper) {
+        return Err(int, "Input value out of bounds.");
     }
-
-    return res->__res.ok;
+    return Ok(int, t);
 }
-
-const char *Err(Result *res) {
-    if (res->err_flag == 0) {
-        _raise(__FILE__, "none()", __LINE__, "Result not Err.");
-    }
-
-    return res->__res.err;
-}
-
-Result get_result() {
-    union ResultU r = { .ok = 10 };
-    Result res = { .__res = r, .err_flag = 0 };
-    return res;
-}
-
-// struct Result {
-//     // bool (*is_err)(void);
-//     union _Result res;
-// };
-
-// union Result {
-//     int ok;
-//     char err[256];
-// };
-
-
 
 int main(void) {
-    printf("Hello, world...\n");
-    Result res = get_result();
-    printf("%d\n", Ok(&res));
-    printf("%s", Err(&res));
+    Result(int) month = read_int_range(1, 12);
+    while (is_err(month)) {
+        printf("Keep range between 1-12. ");
+        month = read_int_range(1, 12);
+    }
+    printf("%d\n", unwrap(int, &month));
 }
