049d3fbe0ae6181fb726666e57ea7580fe572f66
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2023-02-04 03:05:56 -0600

    Fixed naming convention

--- main.c
+++ main.c
@@ -12,51 +12,51 @@
     #define strncpy(...) strncpy_s(__VA_ARGS__)
 #endif
 
-#define _str(expr) #expr
-#define str(expr) _str(expr)
+#define str_(expr) #expr
+#define str(expr) str_(expr)
 
-void _exit_from_raise() {
+void exit_from_raise_() {
     printf("Exiting with errno %d", errno);
     exit(errno);
 }
 
-void _raise(const char *filename, const char *trace, int line, const char *reason) {
+void raise_(const char *filename, const char *trace, int line, const char *reason) {
     fprintf(stderr, "Error raised in %s:%s at line %d: %s\n\terrno = %d\n", 
     filename, 
     trace, 
     line, 
     reason,
     errno);
-    _exit_from_raise();
+    exit_from_raise_();
 }
 
-#define __READ_INT_READLEN 1
-#define __READ_DOUBLE_READLEN 1
-#define __READ_CHAR_READLEN 1
+#define READ_INT_READLEN__ 1
+#define READ_DOUBLE_READLEN__ 1
+#define READ_CHAR_READLEN__ 1
 
-inline int _read_int(FILE *p, int _line, const char *_filename) {
+inline int read_int_(FILE *p, int line_, const char *filename_) {
     int d;
     int res = fscanf(p, "%d", &d);
-    if (res != __READ_INT_READLEN) {
-        _raise(_filename, "read_int(FILE *)", _line, "scanf_s read failure.");
+    if (res != READ_INT_READLEN__) {
+        raise_(filename_, "read_int(FILE *)", line_, "scanf_s read failure.");
     }
     return d;
 }
 
-inline double _read_double(FILE *p, int _line, const char *_filename) {
+inline double read_double_(FILE *p, int line_, const char *filename_) {
     double lf;
     int res = fscanf(p, "%lf", &lf);
-    if (res != __READ_DOUBLE_READLEN) {
-        _raise(_filename, "read_double(FILE *)", _line, "fscanf read failure.");
+    if (res != READ_DOUBLE_READLEN__) {
+        raise_(filename_, "read_double(FILE *)", line_, "fscanf read failure.");
     }
     return lf;
 }
 
-inline char _read_char(FILE *p, int _line, const char *_filename) {
+inline char _read_char(FILE *p, int line_, const char *filename_) {
     char c;
     int res = fscanf(p, "%c", &c);
-    if (res != __READ_CHAR_READLEN) {
-        _raise(_filename, "read_char(FILE *)", _line, "fscanf read failure.");
+    if (res != READ_CHAR_READLEN__) {
+        raise_(filename_, "read_char(FILE *)", line_, "fscanf read failure.");
     }
     return c;
 }
@@ -66,13 +66,13 @@ inline char _read_char(FILE *p, int _line, const char *_filename) {
 /// Raises:
 /// Function will raise an error if a 32-bit integer cannot be read
 /// from the stream.
-#define read_int_f(filep) _read_int(filep, __LINE__, __FILE__)
+#define read_int_f(filep) read_int_(filep, __LINE__, __FILE__)
 /// Attempts to read a double from a stream.
 ///
 /// Raises:
 /// Function will raise an error if a double cannot be read
 /// from the stream.
-#define read_double_f(filep) _read_double(filep, __LINE__, __FILE__)
+#define read_double_f(filep) read_double_(filep, __LINE__, __FILE__)
 /// Attempts to read a char from a stream.
 ///
 /// Raises:
@@ -80,7 +80,7 @@ inline char _read_char(FILE *p, int _line, const char *_filename) {
 /// from the stream.
 ///
 /// Note: This is not to be confused with a byte.
-#define read_char_f(filep) _read_char(filep, __LINE__, __FILE__)
+#define read_char_f(filep) read_char_(filep, __LINE__, __FILE__)
 
 /// Attempts to read an int from stdin.
 ///
@@ -105,5 +105,5 @@ inline char _read_char(FILE *p, int _line, const char *_filename) {
 
 int main(void) {
     errno = 5;
-    _raise(__FILE__, "main(void)", __LINE__, "Arbitrary error call.");
+    raise_(__FILE__, "main(void)", __LINE__, "Arbitrary error call.");
 }
